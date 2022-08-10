955b6a47c56d16b92a427e9d120ca3de9835d275
William Sun <william.sun@volvo.com>
2022-08-10 14:49:17 +0800

    Add project files.

---
+++ MyTravel.portal/Areas/Blog/Controllers/PageController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyTravel.portal.Areas.Blog.Controllers
+{
+    public class PageController : Controller
+    {
+        // GET: Blog/Page
+        public ActionResult Home()
+        {
+            return View("~/Areas/Blog/Views/Page/Home.cshtml");
+        }
+
+        public ActionResult News()
+        {
+            return View("~/Areas/Blog/Views/Page/News.cshtml");
+        }
+
+        public ActionResult Contacts()
+        {
+            return View("~/Areas/Blog/Views/Page/Contacts.cshtml");
+        }
+
+        public ActionResult About()
+        {
+            return View("~/Areas/Blog/Views/Page/About.cshtml");
+        }
+    }
+}
